66da52c5f93672729825fc692bbefda0875b8ce3
Nicholas Ingolia <nick@ingolia.org>
2020-04-06 01:00:04 -0700

    Abstracted quality trimming into structs

--- src/trimrs/qual_trim.rs
+++ src/trimrs/qual_trim.rs
@@ -19,53 +19,89 @@
 //! The algorithm is taken from `Cutadapt`, which in turn took it from
 //! `BWA`.
 
-/// Computes read length remaining after dropping low-quality bases
-/// from the end.
-///
-/// The remaining length may range from `0` to `quals.len()`
-/// inclusive.
-///
-/// # Arguments
-/// * `threshold` is the quality threshold
-/// * `quals` are the qualities
-pub fn qual_cutoff_end(threshold: u8, quals: &[u8]) -> usize {
-    let mut running_sum = 0;
-    let mut lowest_score = 0;
-    let mut lowest_offset = quals.len();
-    let threshold_i = threshold as isize;
+use serde::{Serialize, Deserialize};
 
-    for i in (0..quals.len()).rev() {
-        running_sum += quals[i] as isize - threshold_i;
+use crate::trimming::Trimming;
 
-        if running_sum > 0 {
-            break;
-        } else if running_sum <= lowest_score {
-            lowest_score = running_sum;
-            lowest_offset = i;
-        }
-    }
-
-    return lowest_offset;
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Hash, Serialize, Deserialize)]
+pub struct QualTrimEnd {
+    threshold_i: isize,
 }
 
-pub fn qual_cutoff_start(threshold: u8, quals: &[u8]) -> usize {
-    let mut running_sum = 0;
-    let mut lowest_score = 0;
-    let mut lowest_offset = 0;
-    let threshold_i = threshold as isize;
-
-    for i in 0..quals.len() {
-        running_sum += quals[i] as isize - threshold_i;
-
-        if running_sum > 0 {
-            break;
-        } else if running_sum <= lowest_score {
-            lowest_score = running_sum;
-            lowest_offset = i + 1;
+impl QualTrimEnd {
+    pub fn new(threshold: u8) -> Self {
+        QualTrimEnd { threshold_i: threshold as isize }
+    }
+    
+    /// Computes read length remaining after dropping low-quality bases
+    /// from the end.
+    ///
+    /// The remaining length may range from `0` to `quals.len()`
+    /// inclusive.
+    ///
+    /// # Arguments
+    /// * `threshold` is the quality threshold
+    /// * `quals` are the qualities
+    pub fn remaining(&self, quals: &[u8]) -> usize {
+        let mut running_sum = 0;
+        let mut lowest_score = 0;
+        let mut lowest_offset = quals.len();
+        
+        for i in (0..quals.len()).rev() {
+            running_sum += quals[i] as isize - self.threshold_i;
+            
+            if running_sum > 0 {
+                break;
+            } else if running_sum <= lowest_score {
+                lowest_score = running_sum;
+                lowest_offset = i;
+            }
         }
+        
+        return lowest_offset;
     }
 
-    return lowest_offset;
+    pub fn trim(&self, trimming: &mut Trimming) {
+        if let Some(quals) = trimming.qual_trimmed() {
+            trimming.trim_from_end(self.remaining(quals));
+        }
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Hash, Serialize, Deserialize)]
+pub struct QualTrimStart {
+    threshold_i: isize,
+}
+
+impl QualTrimStart {
+    pub fn new(threshold: u8) -> Self {
+        QualTrimStart { threshold_i: threshold as isize }
+    }
+
+    pub fn remaining(&self, quals: &[u8]) -> usize {
+        let mut running_sum = 0;
+        let mut lowest_score = 0;
+        let mut lowest_offset = 0;
+
+        for i in 0..quals.len() {
+            running_sum += quals[i] as isize - self.threshold_i;
+
+            if running_sum > 0 {
+                break;
+            } else if running_sum <= lowest_score {
+                lowest_score = running_sum;
+                lowest_offset = i + 1;
+            }
+        }
+        
+        return lowest_offset;
+    }
+
+    pub fn trim(&self, trimming: &mut Trimming) {
+        if let Some(quals) = trimming.qual_trimmed() {
+            trimming.trim_from_start(self.remaining(quals));
+        }
+    }
 }
 
 // Cutadapt example for end trimming
@@ -82,57 +118,61 @@ mod tests {
 
     #[test]
     fn quality_trim_end() {
-        assert_eq!(qual_cutoff_end(10, &vec![]), 0);
+        let qual_trim = QualTrimEnd::new(10);
         
-        assert_eq!(qual_cutoff_end(10, &vec![9]), 0);
-        assert_eq!(qual_cutoff_end(10, &vec![10]), 0);
-        assert_eq!(qual_cutoff_end(10, &vec![11]), 1);
+        assert_eq!(qual_trim.remaining(&vec![]), 0);
+        
+        assert_eq!(qual_trim.remaining(&vec![9]), 0);
+        assert_eq!(qual_trim.remaining(&vec![10]), 0);
+        assert_eq!(qual_trim.remaining(&vec![11]), 1);
 
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 9]), 2);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 10]), 2);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 11]), 3);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 9]), 2);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 10]), 2);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 11]), 3);
 
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 9, 20]), 4);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 10, 20]), 4);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 11, 20]), 4);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 9, 20]), 4);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 10, 20]), 4);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 11, 20]), 4);
 
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 10, 9, 10]), 2);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 11, 9, 10]), 3);
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 11, 9, 9]), 3);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 10, 9, 10]), 2);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 11, 9, 10]), 3);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 11, 9, 9]), 3);
 
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 9, 11, 9, 10]), 2);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 9, 11, 9, 10]), 2);
 
-        assert_eq!(qual_cutoff_end(10, &vec![20, 20, 9, 12, 9, 10]), 4);
+        assert_eq!(qual_trim.remaining(&vec![20, 20, 9, 12, 9, 10]), 4);
 
-        assert_eq!(qual_cutoff_end(10, &vec![42, 40, 26, 27, 8, 7, 11, 4, 2, 3]), 4);
+        assert_eq!(qual_trim.remaining(&vec![42, 40, 26, 27, 8, 7, 11, 4, 2, 3]), 4);
     }
 
     #[test]
     fn quality_trim_start() {
-        assert_eq!(qual_cutoff_start(10, &vec![]), 0);
+        let qual_trim = QualTrimStart::new(10);
         
-        assert_eq!(qual_cutoff_start(10, &vec![9]), 1);
-        assert_eq!(qual_cutoff_start(10, &vec![10]), 1);
-        assert_eq!(qual_cutoff_start(10, &vec![11]), 0);
-
-        assert_eq!(qual_cutoff_start(10, &vec![9, 20, 20]), 1);
-        assert_eq!(qual_cutoff_start(10, &vec![10, 20, 20]), 1);
-        assert_eq!(qual_cutoff_start(10, &vec![11, 20, 20]), 0);
+        assert_eq!(qual_trim.remaining(&vec![]), 0);
         
-        assert_eq!(qual_cutoff_start(10, &vec![20,  9, 20, 20]), 0);
-        assert_eq!(qual_cutoff_start(10, &vec![20, 10, 20, 20]), 0);
-        assert_eq!(qual_cutoff_start(10, &vec![20, 11, 20, 20]), 0);
+        assert_eq!(qual_trim.remaining(&vec![9]), 1);
+        assert_eq!(qual_trim.remaining(&vec![10]), 1);
+        assert_eq!(qual_trim.remaining(&vec![11]), 0);
 
-        assert_eq!(qual_cutoff_start(10, &vec![10, 9, 10, 20, 20]), 3);
-        assert_eq!(qual_cutoff_start(10, &vec![10, 9, 11, 20, 20]), 2);
-        assert_eq!(qual_cutoff_start(10, &vec![ 9, 9, 11, 20, 20]), 2);
+        assert_eq!(qual_trim.remaining(&vec![9, 20, 20]), 1);
+        assert_eq!(qual_trim.remaining(&vec![10, 20, 20]), 1);
+        assert_eq!(qual_trim.remaining(&vec![11, 20, 20]), 0);
+        
+        assert_eq!(qual_trim.remaining(&vec![20,  9, 20, 20]), 0);
+        assert_eq!(qual_trim.remaining(&vec![20, 10, 20, 20]), 0);
+        assert_eq!(qual_trim.remaining(&vec![20, 11, 20, 20]), 0);
 
-        assert_eq!(qual_cutoff_start(10, &vec![10, 9, 11, 9, 20, 20]), 4);
+        assert_eq!(qual_trim.remaining(&vec![10, 9, 10, 20, 20]), 3);
+        assert_eq!(qual_trim.remaining(&vec![10, 9, 11, 20, 20]), 2);
+        assert_eq!(qual_trim.remaining(&vec![ 9, 9, 11, 20, 20]), 2);
 
-        assert_eq!(qual_cutoff_start(10, &vec![10, 9, 12, 9, 20, 20]), 2);
+        assert_eq!(qual_trim.remaining(&vec![10, 9, 11, 9, 20, 20]), 4);
+
+        assert_eq!(qual_trim.remaining(&vec![10, 9, 12, 9, 20, 20]), 2);
 
         
-        assert_eq!(qual_cutoff_start(10, &vec![3, 2, 4, 11, 7, 8, 27, 26, 40, 42]), 6);
+        assert_eq!(qual_trim.remaining(&vec![3, 2, 4, 11, 7, 8, 27, 26, 40, 42]), 6);
     }
 
 }
